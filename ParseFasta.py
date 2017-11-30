da73c490dc28698c4d67c9046321eb43c89d74c7
junhaobearxiong <jxiong3@jhu.edu>
2017-11-30 16:49:02 -0500

    started benchmark

--- ParseFasta.py
+++ ParseFasta.py
@@ -24,50 +24,57 @@ total_kmer_count = 0
 sketch = SimpleSketches(MAX_SIZE_1)
 
 ''' User Set Parameters '''
-kmer_length = int(input('Please specify the length of each k-mer: '))
-sketch_percent = float(input('Please specify the percentage of the total number of '
-    'k-mers you wish to include in the sketch: '))
-#TODO: exception handling
-if kmer_length > 80: # when the length of kmer exceeds the length of a line
-# reading input becomes hard
-    raise ValueError("Kmer length is too long")
+def set_param():
+	kmer_length = int(input('Please specify the length of each k-mer: '))
+	sketch_percent = float(input('Please specify the percentage of the total number of '
+		'k-mers you wish to include in the sketch: '))
+	#TODO: exception handling
+	if kmer_length > 80: # when the length of kmer exceeds the length of a line
+	# reading input becomes hard
+		raise ValueError("Kmer length is too long")
 
+def compare_genomes():
+	#TODO need to complete kmer that spans multiple line
+	# iterate over the command line arguments
+	# open each file and process
+	for index, arg in enumerate(sys.argv[1:]):
+		print("Currently processing file {}".format(arg))
+		with open(arg) as fi:
+			temp = '' # used to store the part of the line that doesn't form kmer
+			# with the string of the current line
+			for i, line in enumerate(fi):
+				# preprocessing the input
+				line.strip('\n')
+				if line[0] == '>':
+					continue
 
-#TODO need to complete kmer that spans multiple line
-# iterate over the command line arguments
-# open each file and process
-for index, arg in enumerate(sys.argv[1:]):
-    print("Currently processing file {}".format(arg))
-    with open(arg) as fi:
-        temp = '' # used to store the part of the line that doesn't form kmer
-        # with the string of the current line
-        for i, line in enumerate(fi):
-            # preprocessing the input
-            line.strip('\n')
-            if line[0] == '>':
-                continue
-
-            line = temp + line # add the buffer string from the previous line
-            # to the beginning of the current line
-            # go through each starting position of kmer
-            for j in range(0, len(line) - kmer_length):
-                total_kmer_count += 1
-                r = random.random() # generate a pseudo random number in [0, 1)
-                if (r <= sketch_percent):
-                    # if the random number is within the probablity of
-                    # including a kmer
-                    # we add it to our sketch
-                    kmer = line[j:j+kmer_length]  # grab the kmer
-                    sketch.addKmer(kmer)
-            temp = line[len(line) - kmer_length:]
-    # finish processing the first genome
-    if (index == 0):
-        sketch.endFirstGenome(MAX_SIZE_2)
+				line = temp + line # add the buffer string from the previous line
+				# to the beginning of the current line
+				# go through each starting position of kmer
+				for j in range(0, len(line) - kmer_length):
+					total_kmer_count += 1
+					r = random.random() # generate a pseudo random number in [0, 1)
+					if (r <= sketch_percent):
+						# if the random number is within the probablity of
+						# including a kmer
+						# we add it to our sketch
+						kmer = line[j:j+kmer_length]  # grab the kmer
+						sketch.addKmer(kmer)
+				temp = line[len(line) - kmer_length:]
+		# finish processing the first genome
+		if (index == 0):
+			sketch.endFirstGenome(MAX_SIZE_2)
 
 
 
 #sketch.printSketches()
-print("Total kmer count of both genomes are {}".format(total_kmer_count))
-print("Total # of kmer in common is {}".format(sketch.getCommon()))
-print("Total # of kmer in both sketches is {}".format(sketch.getTotalSizeofSketches()))
-print("Distance between the two is {}".format(calculateDistance(sketch, kmer_length, sketch_percent)))
+def print_result():
+	print("Total kmer count of both genomes are {}".format(total_kmer_count))
+	print("Total # of kmer in common is {}".format(sketch.getCommon()))
+	print("Total # of kmer in both sketches is {}".format(sketch.getTotalSizeofSketches()))
+	print("Distance between the two is {}".format(calculateDistance(sketch, kmer_length, sketch_percent)))
+
+''' Main '''
+set_param()
+compare_genomes()
+print_result()
