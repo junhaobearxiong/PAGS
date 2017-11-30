537f50d9bab93931f1107f6312eb731f48168464
junhaobearxiong <jxiong3@jhu.edu>
2017-11-30 18:13:12 -0500

    benchmark finished

--- DistanceCalculation.py
+++ DistanceCalculation.py
@@ -1,8 +1,11 @@
 import math
 
 def calculateDistance(sketch, kmer_length, sketch_percent) :
-    common = sketch.getCommon()
-    size = sketch.getTotalSizeofSketches()
-    return (-1 * math.log((2 * common + .0)/size/sketch_percent))/kmer_length
+	common = sketch.getCommon()
+	if common == 0:
+		print("0 common kmers between sketches. Run with smaller k or larger p")
+		exit()
+	size = sketch.getTotalSizeofSketches()
+	return (-1 * math.log((2 * common + .0)/size/sketch_percent))/kmer_length
     #return (-1 * math.log((2 * common + .0)/size))/kmer_length
     
