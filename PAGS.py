2ef3301cee0566a2bdf3312cf02a4e9b2f51247f
junhaobearxiong <jxiong3@jhu.edu>
2017-12-02 11:23:16 -0500

    resolved merge conflict

--- PAGS.py
+++ PAGS.py
@@ -7,7 +7,6 @@ from SimpleSketches import SimpleSketches
 from HashSketches import HashSketches
 from DistanceCalculation import calculateDistance
 
-
 class PAGS:
 	def __init__(self):
 		''' Attributes '''
@@ -62,6 +61,8 @@ class PAGS:
 
 					line = temp + line # add the buffer string from the previous line
 					# to the beginning of the current line
+					temp = self.get_subsequence(line, temp, spaced_length=1)
+					'''
 					# go through each starting position of kmer
 					for j in range(0, len(line) - self.kmer_length):
 						self.total_kmer_count += 1
@@ -73,6 +74,7 @@ class PAGS:
 							kmer = line[j:j+self.kmer_length]  # grab the kmer
 							self.sketch.addKmer(kmer)
 					temp = line[len(line) - self.kmer_length:]
+					'''
 			# finish processing the first genome
 			if (index == 0):
 				self.sketch.endFirstGenome(self.MAX_SIZE_2)
@@ -81,6 +83,36 @@ class PAGS:
 		dist = calculateDistance(self.sketch, self.kmer_length, self.sketch_percent)
 		return dist
 
+	def get_subsequence(self, line, temp, spaced_length=0):
+		'''
+		This helper function extrcts subsequences from fasta lines.
+		Args:
+			line (str): the processed line from the input file.
+			total_kmer_count (int): keep track of total kmer count
+			spaced_length (int): the gap length between each pair of character in the subsequence
+
+		The script loops through each line and extracts subsequence to store in the hash. The spaced
+		length is initialized as 1 so that it will extract consecutive kmers by default. If the 
+		coverage string, the substring ranging from the first requested character in the subsequence 
+		to the last character, exceeds the length of the line, the function raises an error.
+		'''
+		chunk_length = self.kmer_length + (self.kmer_length - 1) * spaced_length
+		if chunk_length > len(line):
+			raise ValueError("The subsequence length is too long")
+		for j in range(0, len(line) - chunk_length):
+			self.total_kmer_count += 1
+			r = random.random() # generate a pseudo random number in [0, 1)
+			if (r <= self.sketch_percent):
+				# if the random number is within the probablity of
+				# including a kmer
+				# we add it to our sketch
+				chunk = line[j:j + chunk_length]
+				# attain the subsequence
+				subsequence = chunk[::spaced_length + 1] 
+				self.sketch.addKmer(subsequence)
+		temp = line[len(line) - chunk_length:]
+		return temp
+	
 	#sketch.printSketches()
 	def print_result(self):
 		print("Total kmer count of both genomes are {}".format(self.total_kmer_count))
