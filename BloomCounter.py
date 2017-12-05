1df4dd3d30e97f1f0252f268edbe37fd077bcf89
MackZackA <zsong18@illinois.edu>
2017-12-05 23:38:04 +0100

    updated Bloom Counter

--- BloomCounter.py
+++ BloomCounter.py
@@ -1,19 +1,9 @@
-
-<<<<<<< HEAD
 def hash_function(item):
     h = hash(item)
     return int(1 << (h%64)) | int(1 << (h//64%64))
 
 def mask(val):
     return bin(hash_function(val))[2:]
-=======
-def hashfn(item):
-    h = hash(item)
-    return (1 << (h%64)) | (1 << (h/64%64))
-
-def mask(val):
-    return bin(hashfn(val))[2:]
->>>>>>> 5f4cea53435f320a39a8b64b2f62b560e22468fe
 
 class CountingBloom(object):
     def __init__(self):
@@ -35,21 +25,5 @@ class CountingBloom(object):
             if bit == '1' and self.items[index]:
                 self.items[index] -= 1
 
-<<<<<<< HEAD
-def remove_singletons(counting_bloom):
-
-
-=======
->>>>>>> 5f4cea53435f320a39a8b64b2f62b560e22468fe
 bloom = CountingBloom()
 args = ('foo', 'bar', 'baz')
-for arg in args:
-    bloom.add(arg)
-<<<<<<< HEAD
-print(', '.join(str(bloom.query(arg)) for arg in args))
-=======
-    print(', '.join(str(bloom.query(arg)) for arg in args))
->>>>>>> 5f4cea53435f320a39a8b64b2f62b560e22468fe
-for arg in args:
-    bloom.remove(arg)
-print(', '.join(str(bloom.query(arg)) for arg in args))
